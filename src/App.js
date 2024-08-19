a17ef3faf741ca7680d5af6b42fa2f8470ac9c58
anirudhgeek21 <anirudhprajapati2466@gmail.com>
2024-08-19 10:16:50 +0530

    initial 2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home.jsx';
 import First from './components/first/one';
+import Second from './components/second/two';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/first" element={<First />} />
+          <Route path="/second" element={<Second />} />
         </Routes>
       </div>
     </Router>
--- src/components/second/two.jsx
+++ src/components/second/two.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
-import one from "./one.png";
 
-const First = () => {
+const Second = () => {
     const [answer, setAnswer] = useState('');
+    const [obfuscatedKey, setObfuscatedKey] = useState(''); 
     const navigate = useNavigate();
     const [bgColor, setBgColor] = useState('bg-black');
 
@@ -17,21 +17,36 @@ const First = () => {
     };
 
     return (
-        <motion.div
+        <>
+         <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 1 }}
             className="relative flex flex-col items-center justify-center bg-black h-screen font-mono"
-        >
-            <p className="text-white mt-0">Hahahahaha you have a <span>bright</span> future but can’t be determined in terms of total numbers.</p>
-            <img src={one} className="w-[60%] mt-10 pb-10" alt="One" />
+         >
+            <p className='md:block sm:hidden text-white'>
+                Sometimes small things are the things that need to be noticed 
+            </p>
+
+            <p className='text-transparent'>
+                key to Akhet is :- <span className="text-black">the path you already visited</span>
+            </p>
+
+            <p className='hidden'>
+                https://en.wikipedia.org/wiki/Akhet_(hieroglyph)
+            </p>
+
+            <p className='md:hidden sm:block text-white'>
+                The answer lies in the void 
+            </p>
 
             <div className="absolute bottom-10 right-10 flex items-center justify-end">
                 <input
                     type="text"
                     value={answer}
                     onChange={(e) => setAnswer(e.target.value)}
-                    className={`rounded ${bgColor} border border-[0.1px] border-slate-600 text-black`}
+                    placeholder='let the sun rise'
+                    className={`rounded ${bgColor} border border-[0.1px] pl-5 border-slate-600 text-black`}
                 />
                 <button
                     onClick={handleNextClick}
@@ -40,8 +55,9 @@ const First = () => {
                     go
                 </button>
             </div>
-        </motion.div>
+         </motion.div>
+        </>
     );
 };
 
-export default First;
+export default Second;
